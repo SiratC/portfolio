992d3a35bf1352007408c7d0fd3d358df1f22cd0
Sirat <127055475+SiratC@users.noreply.github.com>
2025-04-03 20:30:27 -0400

    Update LastUpdated.js

--- src/sections/LastUpdated.js
+++ src/sections/LastUpdated.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const LastUpdated = () => {
-  const buildDate = process.env.REACT_APP_BUILD_DATE || new Date().toISOString().slice(0, 10);
-  return <p id ="update">Site last updated {buildDate}</p>;
+  
+  const buildDate = process.env.REACT_APP_BUILD_DATE;
+
+  return (
+    <p id="update">
+      Site last updated {buildDate ? buildDate : "Date not set"}
+    </p>
+  );
 };
 
 export default LastUpdated;
