b5688df4d2ff7f238987859c8360ae2afa69cb57
Sirat <127055475+SiratC@users.noreply.github.com>
2025-04-07 14:21:18 -0400

    Update TechIcon.js

--- src/sections/TechIcon.js
+++ src/sections/TechIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const TechIcon = ({ iconName, iconLabel, color }) => {
-  // Remove the '#' if it's included and convert the iconName to lowercase
+
   const processedColor = color.startsWith('#') ? color.slice(1) : color;
   const url = `https://cdn.simpleicons.org/${iconName.toLowerCase()}/${processedColor}`;
 
