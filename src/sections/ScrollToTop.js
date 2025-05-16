478d1817662d67e6e75d08924d7413cef63fc17c
Sirat <127055475+SiratC@users.noreply.github.com>
2025-05-15 23:36:15 -0400

    Update ScrollToTop.js

--- src/sections/ScrollToTop.js
+++ src/sections/ScrollToTop.js
@@ -6,23 +6,22 @@ export default function ScrollToTop() {
   const isFirst = useRef(true);
 
   useEffect(() => {
-    // On the very first mount…
+
     if (isFirst.current) {
       isFirst.current = false;
       if (hash) {
-        // If URL has a hash, scroll to it
+    
         const id = hash.slice(1);
         const el = document.getElementById(id);
         if (el) {
-          // slight delay to ensure the element exists
+        
           setTimeout(() => el.scrollIntoView(), 0);
         }
       }
-      // else do nothing (let browser restore position)
+    
       return;
     }
 
-    // After first mount…
     if (hash) {
       const id = hash.slice(1);
       const el = document.getElementById(id);
